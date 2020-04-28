866ff149765d86810414ef45c0700a196d4bf060
Stephen Byers <sabyers1@gmail.com>
2020-04-28 11:56:39 -0400

    Updated print to 3.+ print() format, header

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,4 +1,7 @@
+#!/bin/env python
 #fizz Buzz
+""" Silly fizz buzz program as a demo for conditionals. """
+
 results = {'Fizz':0,
     'Buzz':0,
     'Fizz buzz':0,
@@ -7,16 +10,16 @@ results = {'Fizz':0,
 
 for num in range(1,101):
     if (num % 5 == 0) and (num % 3 == 0):
-        print "Fizz buzz"
+        print ("Fizz buzz")
         results['Fizz buzz']+=1
     elif num % 5 == 0:
-        print "Fizz"
+        print ("Fizz")
         results['Fizz']+=1
     elif num % 3 == 0:
-        print "Buzz"
+        print ("Buzz")
         results['Buzz']+=1
     else:
-        print num
+        print (num)
         results['Other']+=1
 
 print (results)
