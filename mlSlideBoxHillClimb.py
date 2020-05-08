47971bf56d0bc70136312d6e3553018db4e6d9cd
Stephen Byers <sabyers1@gmail.com>
2020-05-08 17:36:00 -0400

    Corrected for tuple so not infinite loop.

--- mlSlideBoxHillClimb.py
+++ mlSlideBoxHillClimb.py
@@ -52,24 +52,24 @@ class TileBox (object):
 
     def __init__(self, object):
         self.mTB = object
-        i=j=0  # row = col = 0
-        # iterate through matrix look for zero value
-        while (self.mTB[i][j] != 0):
-            j+=1 #next col
-            if (j > 2):
-                i+=1  #next row
-                j=0   #reset col
-                if (i > 2):
-                    i=-1  # not found
-                    break
+        self.mloc = -1
 
-        self.mloc = (i+1)+(3*j)
-        self.calc_signature()
+        #iterate through matrix to find zero location
+        for i in range (len(self.mTB)):
+            for j in range(len(self.mTB[0])):
+                if self.mTB[i][j] == 0:             # check for zero
+                    self.mloc = (i+1)+(3*j)         # calculate the index within matrix as if linear array
+                    break
+            if self.mloc > 0:       # found zero
+                break
+
+        self.calc_signature()           # save the 'signature' for faster comparision in lists
     
     def show(self):
-        print ("{}\n{}\n{}\nSig: {}\n".format(self.mTB[0],self.mTB[1],self.mTB[2],self.signature))
+        print ("{}\n{}\n{}\nSig: {:09d} Delta: {}\n"
+            .format(self.mTB[0],self.mTB[1],self.mTB[2],self.signature,TileBoxes_Compare(self)))
 
-    #make long integer describing the 3x3 matrix to represent states in compact form.
+    #make long integer describing the 3x3 matrix to represent states in more compact form.
     def calc_signature(self):
         self.signature  = 100000000 * self.mTB[0][0] 
         self.signature += 10000000 * self.mTB[0][1]
@@ -88,8 +88,8 @@ class TileBox (object):
             tmp = self.mTB[int((loc-1)/3)][(loc-1)%3]
             self.mTB[int((loc-1)/3)][(loc-1)%3] = self.mTB[int((loc-1+idx)/3)][(loc-1+idx)%3]
             self.mTB[int((loc+idx-1)/3)][(loc-1+idx)%3] = tmp
-            print("Swapping: M{},{} with M{},{}\n"
-                .format(int((loc-1)/3),(loc-1)%3, int((loc-1+idx)/3), (loc-1+idx)%3))
+            #print("Swapping: M{},{} with M{},{}\n"
+            #    .format(int((loc-1)/3),(loc-1)%3, int((loc-1+idx)/3), (loc-1+idx)%3))
             self.calc_signature()  # Update signature
         else:
             print("ERROR**Invalid swap request: at loc: {} dir: {}".format(loc,dir))
@@ -99,14 +99,15 @@ class TileBox (object):
         # Given a location in matrix and direction return whether OK to slide.
         # needs to be more elegant method to calculate acceptable moves!!
         # must be at empty (zero) value location and within boundaries
-        slide_rules = {'up':range(4,10),
+        slide_rules = {
+                    'up':range(4,10),
                     'dn':range(1,7),
-                    'rt':{1,2,4,5,7,8},
-                    'lf':{2,3,5,6,8,9}}
+                    'rt':{1,2,4,5,7,8}, #[x for x in range(1,10) if x%3 != 0]
+                    'lf':{2,3,5,6,8,9}} #[y for y in range(1,10) if (y-1)%3 !=0]
         #print("valid_slides for {} are {}\n".format(dir,slide_rules.get(dir)))
-        if loc != self.mloc:
+        if loc != self.mloc:        #check for zero location
             return False
-        if slide_rules.get(dir) == None:
+        if slide_rules.get(dir) == None:  # didn't use correct direction string
             return False
         return bool(loc in slide_rules.get(dir))
 
@@ -120,14 +121,15 @@ class TileBox (object):
         if self.valid_slide(dir,self.mloc):
             self.swap(dir,self.mloc)
             self.mloc = self.mloc + slide_offset(dir)
-            print("Zero moved to loc: {}".format(self.mloc))
+            #print("Zero moved to loc: {}".format(self.mloc))
             return True
         else:
-            print("ERROR**Invalid slide {} at {}".format(dir,self.mloc))
+            #print("ERROR**Invalid slide {} at {}".format(dir,self.mloc))
             return False
 
 def slide_offset(dir):
-    """ Slide uses dictionary to perform mapping of empty tile movement """
+    """ Slide uses dictionary to perform mapping of empty tile movement.
+        Assumes a 3x3 matrix. """
     switcher = {
         'up':-3,
         'dn':3,
@@ -137,7 +139,8 @@ def slide_offset(dir):
     return switcher.get(dir,0) # if not valid return 0
 
 # take the difference between the two matrixes to compare for start.
-def TileBoxes_Compare(tb1, tb2):
+# could get rid of remainder matrix for refactring.
+def TileBox_Compare(tb1, tb2):
     rTB = TileBox([[0,0,0],[0,0,0],[0,0,0]])
     sum = 0
     for i in range (len(tb1.mTB)):
@@ -147,8 +150,8 @@ def TileBoxes_Compare(tb1, tb2):
 
     return sum
 
-def TileBox_Compare(tb1):
-    return TileBoxes_Compare(tb1,fs)
+def TileBoxes_Compare(tb1):
+    return TileBox_Compare(tb1,fs)
 
 # Hill-climbing search algorithm
 # 1. Create two lists, L and Lseen.
@@ -161,60 +164,84 @@ def TileBox_Compare(tb1):
 # 4. Transfer n from L into the list, Lseen, of the states that have been investigated.
 # 5. If L is empty, stop and report failure, otherwise loop to step 2.
         
+#fs = TileBox([[1,2,3],[8,0,4],[7,6,5]])  # final state of matrix
+#ss = TileBox([[0,2,1],[6,7,4],[3,8,5]])  # starting state of matrix
 
 if __name__ == "__main__":
-    fs = TileBox([[1,2,3],[8,0,4],[7,6,5]])
-    ss = TileBox([[0,2,1],[6,7,4],[3,8,5]])
+    fs = TileBox([[1,2,3],[8,0,4],[7,6,5]])  # final state of matrix
+    ss = TileBox([[0,2,1],[6,7,4],[3,8,5]])  # starting state of matrix
     fs.show()
     ss.show()
     #print("The difference between matrixes is: {}".format(TileBox_Compare(fs,ss)))
     #print("The difference between matrixes is: {}".format(TileBox_Compare(ss,ss)))
 
-    L = [ss]
-    Ls = []
-    LsSigs = []
-    maxLoops = 11
-    for xTB in L:
-        nTB=copy.deepcopy(xTB)
-        if TileBox_Compare(nTB) == 0:
-            print("Found final state!")
-            break
-        else:
-            #generate Lseen signature list
-            for tTB in Ls:
-                LsSigs.insert(0,tTB.signature)
+    L = [ss]                # create L list and initalize with random starting state
+    Ls = []                 # create Lseen list
+    LsSigs = []             # list of state signatures for faster comparision
+    maxLoops = 10000          # limit number of iterations
+    while (len(L)>0):       # verify still have states to evaluate
+        for nTB in tuple(L):  #get first element, n, of L list (tuple so list doesn't grow during iteration)
 
-        #Build search options, the four possible slide movement directions
-        for d in {'up','dn','rt','lf'}:
-            tTB = copy.deepcopy(nTB)         #save signature & state before slide
-            if tTB.slide(d):  #if valid slide add to state check.
-                if not tTB.signature in LsSigs:  #Discard signature already in Lseen
-                    L.insert(0,tTB)
+            if TileBoxes_Compare(nTB) == 0:         # compare element state with final state
+                print("\nFound final state!")
+                nTB.show()
+                break
+            else:
+                # Apply to n all available search operators
+                
+                # generate Lseen signature list
+                ##for tTB in Ls:
+                ##    LsSigs.insert(0,tTB.signature)
 
-        if len(L)>4 and len(L) < 10:
-            print("List {}".format(L))
-        #sort options and place at beginning of List.  We want options with smallest
-        #comparision value, so it is a reverse sort.
-        L.sort(key = TileBox_Compare, reverse=True)
-        #L.sort(key = TileBox_Compare())
+                #Build search options, the four possible slide movement directions
+                addList = []          # list of state to add for consideration
+                for d in {'up','dn','rt','lf'}:
+                    tTB = copy.deepcopy(nTB)         #save signature & state before slide
+                    if tTB.slide(d):  #if valid slide add to state check.
+                        if not tTB.signature in LsSigs:  #Discard signature already in Lseen
+                            addList.append(tTB)
 
-        #transfer evaluated item into Lseen and remove from L
-        Ls.insert(0,nTB)
-        try:
-            L.remove(nTB)
-        except:
-            pass
-        if len(L)==0:
-            print("ERROR**No further options avaiable")
-            nTB.show()
-            break
-        maxLoops -= 1
+                #if len(L)>4 and len(L) < 10:
+                #    print("List {}".format(L))
+
+                #sort options and place at beginning of List.  We want options with smallest
+                #comparision value, so it is a reverse sort.
+                if len(addList) > 0:
+                    addList.sort(key = TileBoxes_Compare) # lowest is best, so keep in high-low order
+                    for i in range(len(addList)):   # this loop reverses the sort order (reverse=True)
+                        L.insert(0,addList[i])
+                del addList
+
+                #transfer evaluated item into Lseen and remove from L
+                L.remove(nTB)
+                Ls.append(nTB)
+                maxLoops -= 1
+                
+                if len(L)==0:
+                    print("\nERROR** No further options avaiable")
+                    nTB.show()
+                    break
+                maxLoops -= 1
+                if maxLoops%100 == 0:
+                    print(".", end="")
+
         if maxLoops < 0:
+            print("\nERROR** Exceeded maximum loops!")
+            print("List size: {} Evaluated size {}\n".format(len(L),len(Ls)))
+            #print("L[0:4]  {}".format(L[0:4]))
+            #print("Ls[0:4] {}".format(Ls[0:4])) 
+            for t in L[0:4]:
+                t.show()
+            print("----L seen------")
+            for t in Ls[0:4]:
+                t.show()
             break
 
-    print("List is length {}, List,seen is length {}".format(len(L),len(Ls)))
-    for v in Ls[:10]:
-        v.show()
+    if len(L) == 0:
+        print("ERROR** exhausted states without finding final state.")
+
+    #for v in Ls[:10]:
+    #    v.show()
 
 #     ss.swap('rt',1)
 #     ss.show()
